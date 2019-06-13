8162ba3fb7139f0fb581fcbb85eb0ef995c6aaf7
PotatoFormula <PotatoFormula@gmail.com>
2019-06-14 04:55:09 +0800

    fix search bar

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
 function search() {
-    var word = document.getElementById("keyword");
+    var word = document.getElementById("keyword").value;
     window.open('http://www.google.com/#hl=zh-TW&source=hp&q=' + word, 'Google');
 }
